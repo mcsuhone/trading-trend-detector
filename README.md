18e21ad3be44a0f77727c30c1adfc20a1ca4cc25
mcsuhone <mikko.e.suhonen@gmail.com>
2024-10-20 18:06:43 +0300

    Initial commit Setup streamlit project

---
+++ README.md
@@ -0,0 +1,6 @@
+### Installation
+
+1. Create a python virtual environment `python -m venv ~/.venv`
+2. Activate virtual environment `source ~/.venv/bin/activate`
+3. Install dependencies into virtual env `pip install -r requirements.txt`
+4. Run app `streamlit run app.py`
---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def main():
+    st.title('Trading Data Analysis')
+
+    st.write('Welcome to the Trading Data Analysis app!')
+
+    # You can add more Streamlit components here
+
+if __name__ == '__main__':
+    main()
